e5dad64473f733503e991b9042d998485f9fb024
Keith Stewart <kgstewart@gmail.com>
2017-04-29 19:19:25 -0500

    use the latest esri-loader with fix for Electron use

--- src/app/esri-loader.service.ts
+++ src/app/esri-loader.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { isLoaded, bootstrap, dojoRequire } from './esri-loader';
+import { isLoaded, bootstrap, dojoRequire } from 'esri-loader';
 
 @Injectable()
 export class EsriLoaderService {
